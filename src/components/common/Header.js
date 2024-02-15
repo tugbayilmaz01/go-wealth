4434787e6f520fc41783df6bf070e0c6f75ab222
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-15 23:23:58 +0300

    feat(header): header created

---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="background-color flex items-center p-4">
+      <div className="font-extrabold poppins pl-8 pr-12">GoWealth.</div>
+      <div className="flex gap-x-8 poppins text-sm">
+        <Link to="/">Business</Link>
+        <Link to="/">Pricing</Link>
+        <Link to="/">Services</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

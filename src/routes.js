a442f0122d774f48b979956127245c3d9c9676d8
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-16 21:09:10 +0300

    fix(routes): routes updated

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Calculation from "./pages/Calculation";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/calculation",
+    element: <Calculation />,
   },
 
   // TODO :: use layout if needed or remove this lines.

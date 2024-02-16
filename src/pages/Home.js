479a7a5a0413d73471b7f398f703d0f867ba667a
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-16 21:08:43 +0300

    fix(ui): home page ui edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/common/Header";
 import landingPageImage from "../assets/images/save-money.png";
 
@@ -6,23 +7,31 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="background-color h-full flex">
+      <div className="h-screen background-color flex">
         <div className="flex-1">
-          <div className="poppins text-4xl pt-32 pl-10">
+          <div className="poppins text-4xl pt-40 pl-16">
             Manage your money with <span className="font-medium">GoWealth</span>
             .
           </div>
 
-          <div className="poppins text-4xl pl-10 mt-2">
+          <div className="poppins text-4xl pl-16 mt-4">
             Advance your savings.
           </div>
-          <div className="poppins pl-10 mt-6 w-1/3 font-normal">
+          <div className="poppins pl-16 mt-6 w-1/2 font-normal">
             Find out how much effort you spend on the things you want to buy,
-            and spend more sensibly.
+            <span className="text-gray-600"> and spend more sensibly.</span>
           </div>
-          <button className="started-button pl-10">Get Started</button>
+          <Link to="/calculation">
+            <button className="started-button p-4 ml-16 mt-6 rounded-full">
+              Get Started
+            </button>
+          </Link>
         </div>
-        <img src={landingPageImage} alt="Card" className="w-1/3 mt-16 pr-24" />
+        <img
+          src={landingPageImage}
+          alt="home-page-pic"
+          className="h-2/3 mt-14 pr-32"
+        />
       </div>
     </>
   );

09b6c00623af53db90c6fc0977ceccb5caebd839
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-22 15:13:05 +0300

    feat(purchasesTable): data saved to local storage showed in the table

--- src/pages/PurchasesTable.js
+++ src/pages/PurchasesTable.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PurchasesTable = () => {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("purchaseData")) || {};
+    const tableData = Array.isArray(storedData) ? storedData : [storedData];
+
+    setTableData(tableData);
+  }, []);
+
   return (
-    <div>
-      Purchases
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Labor</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
+    <div className="p-12">
+      <table className="border w-full">
+        <thead>
+          <tr className="flex justify-between mx-2">
+            <th>Expense Name</th>
+            <th>Expense Price</th>
+            <th>Effort</th>
+          </tr>
+        </thead>
+        <tbody className="flex flex-col">
+          {tableData.map((item, index) => (
+            <tr key={index} className="border flex justify-between">
+              <td>{item.expenseName}</td>
+              <td>{item.expenditure}</td>
+              <td>{item.effortResult}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );

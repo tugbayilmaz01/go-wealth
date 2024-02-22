7f8a912a190b3153db05b2115f625a9a25784afa
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-22 16:31:52 +0300

    feat(header): added to go to landing page by clicking on gowealth

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
   return (
     <header className="background-color flex justify-between items-center p-1">
       <div className="flex">
-        <div className="font-extrabold poppins pl-8 pr-12">GoWealth.</div>
+        <Link to="/" className="font-extrabold poppins pl-8 pr-12">
+          GoWealth.
+        </Link>
         <div className="header-title flex gap-x-8 poppins text-sm">
           <Link to="/">Business</Link>
           <Link to="/">Pricing</Link>

7a718393d569d973296df267a9d3ba39991662a0
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-23 15:21:57 +0300

    ui(purchaseTable): ui changes & addition of search input

--- src/pages/PurchasesTable.js
+++ src/pages/PurchasesTable.js
@@ -11,10 +11,23 @@ const PurchasesTable = () => {
   }, []);
 
   return (
-    <div className="p-8">
+    <div className="p-5">
+      <div className="flex flex-row justify-between gap-x-2">
+        <p className="poppins p-1 font-semibold">
+          Check out your lately expenses!
+        </p>
+        <div className="flex items-end gap-x-2 mb-2">
+          <input
+            placeholder="Search..."
+            className="px-1.5 py-1 table-search poppins"
+          ></input>
+          <button className="search-button poppins p-1 px-2">Find</button>
+        </div>
+      </div>
+
       <table className="border w-full shadow-md">
         <thead>
-          <tr className="flex justify-between p-1 poppins border text-white bg-gray-900">
+          <tr className="flex justify-between p-2 poppins border text-white bg-gray-900 rounded-md">
             <th className="font-normal">Expense Name</th>
             <th className="font-normal">Expense Price</th>
             <th className="font-normal">Effort</th>
@@ -22,10 +35,13 @@ const PurchasesTable = () => {
         </thead>
         <tbody className="flex flex-col ">
           {tableData.map((item, index) => (
-            <tr key={index} className="flex justify-between poppins mx-2">
-              <td>{item.expenseName}</td>
-              <td>{item.expenditure}</td>
-              <td>{item.effortResult}</td>
+            <tr
+              key={index}
+              className="flex justify-between poppins mx-2 border-b border-inherit"
+            >
+              <td className="p-1">{item.expenseName}</td>
+              <td className="p-1">{item.expenditure}</td>
+              <td className="p-1">{item.effortResult}</td>
             </tr>
           ))}
         </tbody>

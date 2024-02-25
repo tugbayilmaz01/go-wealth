62ccca931e886add3bff61fdd8785e7278c06d8d
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-25 16:14:14 +0300

    ui(home): home page made responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,22 +7,22 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="h-screen background-color flex">
-        <div className="flex-1">
-          <div className="poppins text-4xl pt-40 pl-16">
+      <div className="flex flex-col-reverse md:flex-row gap-x-24">
+        <div className="md:w-1/2">
+          <div className="poppins text-4xl md:pt-40 md:pl-16">
             Manage your money with <span className="font-medium">GoWealth</span>
             .
           </div>
 
-          <div className="poppins text-4xl pl-16 mt-4">
+          <div className="poppins text-4xl md:pl-16 mt-4">
             Advance your savings.
           </div>
-          <div className="poppins pl-16 mt-6 w-1/2 font-normal">
+          <div className="poppins md:pl-16 mt-6 md:w-full font-normal">
             Find out how much effort you spend on the things you want to buy,
             <span className="text-gray-600"> and spend more sensibly.</span>
           </div>
           <Link to="/calculation">
-            <button className="started-button p-4 ml-16 mt-6 rounded-full">
+            <button className="started-button p-4 md:ml-16 mt-6 rounded-full">
               Get Started
             </button>
           </Link>
@@ -30,7 +30,7 @@ const Home = () => {
         <img
           src={landingPageImage}
           alt="home-page-pic"
-          className="h-2/3 mt-14 pr-32"
+          className="flex-shrink-0 h-2/3 mt-12"
         />
       </div>
     </>

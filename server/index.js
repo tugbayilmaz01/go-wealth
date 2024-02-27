e6bfbc3c8e2226757d7b5e2e862745c6497ffb55
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-02-27 15:01:11 +0300

    chore(server): added server and database files

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const { pool } = require("./util/database");
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Connected to the MySQL database pool!");
+    connection.release();
+  } catch (err) {
+    console.error("Error connecting to the database pool", err);
+    process.exit(1);
+  }
+
+  app.listen(port, () => {
+    console.log("Server is up on port " + port);
+  });
+})();

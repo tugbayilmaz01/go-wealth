97c49742cd879e80da14916eaa5f7af0524d168c
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-03-04 13:01:11 +0300

    fix(ui): login with github & google ui fixed

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "../../components/common/Header";
 import googleIcon from "../../assets/images/google.png";
 import githubIcon from "../../assets/images/github.png";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
       <Header currentPage="login" />
       <div className="flex flex-col items-center h-full mt-16">
-        <h2 className="poppins text-2xl mb-6 dark-blue font-medium tracking-wide">
+        <h2 className="poppins text-2xl mb-5 dark-blue font-medium tracking-wide">
           Welcome back to
           <span className="font-bold"> GoWealth.</span>{" "}
         </h2>
@@ -29,13 +30,16 @@ const Login = () => {
           </button>
           <h2 className="poppins font-bold text-color text-center">Or</h2>
           <div className="flex flex-row w-full gap-x-4">
-            <button className="mt-2 p-2 text-center bg-color poppins rounded-xl font-bold w-full">
+            <button className="mt-2 p-2 text-center bg-color poppins rounded-xl font-bold w-full flex items-center justify-center">
               <img src={googleIcon} alt="google-icon" className="w-1/6" />
             </button>
-            <button className="mt-2 p-2 bg-color poppins rounded-xl font-bold w-full">
+            <button className="mt-2 p-2 bg-color poppins rounded-xl font-bold w-full flex items-center justify-center">
               <img src={githubIcon} alt="github-icon" className="w-1/6" />
             </button>
           </div>
+          <h2 className="text-color poppins font-bold">
+            Dont Have An Accont? <Link to="/register">Sign Up</Link>{" "}
+          </h2>
         </div>
         <div
           className="absolute bottom-0 left-0 w-full h-full bg-black"

87b2a9aea8b074f64db4de8f4da824ae675be911
tugbayilmaz01 <tugbayilmaz4@posta.mu.edu.tr>
2024-03-07 22:11:57 +0300

    feat(signup): sign up form edited & form's submit handled

--- src/pages/Authentication/SignUp.js
+++ src/pages/Authentication/SignUp.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Header from "../../components/common/Header";
+import { userSchema } from "../../helpers/validations/UserValidation";
 
 const SignUp = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(false);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const isValid = await userSchema.isValid(formValues);
+    if (isValid) {
+      console.log("isvalid");
+      try {
+        await axios.post("http://localhost:5000/users/register", formValues);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      }
+    }
+  };
   return (
     <>
       <Header currentPage="register" />
@@ -10,34 +38,59 @@ const SignUp = () => {
           Sign up and manage your money with
           <span className="font-bold"> GoWealth.</span>{" "}
         </h2>
-        <div className="flex flex-col gap-y-4 items-center border-black register-bg rounded-xl p-10 relative z-10 w-1/4">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-y-4 items-center border-black register-bg rounded-xl p-10 relative z-10  lg:w-1/4 "
+        >
           <input
-            id="name"
-            placeholder="Name"
             className="p-2 border border-black rounded-md input-bg w-full"
+            id="name"
+            type="text"
+            value={formValues.name}
+            onChange={handleChange}
+            name="name"
+            placeholder="Name"
+            required
           />
           <input
-            id="surname"
-            placeholder="Surname"
-            name="surname"
             className="p-2 border border-black rounded-md input-bg w-full"
+            id="surname"
+            type="text"
+            value={formValues.surname}
+            onChange={handleChange}
+            name="surname"
+            placeholder="Surname"
+            required
           />
 
           <input
-            id="email"
-            placeholder="Email"
             className="p-2 border border-black rounded-md input-bg w-full"
+            id="email"
+            type="email"
+            value={formValues.email}
+            onChange={handleChange}
+            name="email"
+            placeholder="Email"
+            required
           />
           <input
+            className="p-2 border border-black rounded-md input-bg w-full"
             id="password"
             placeholder="Password"
+            name="password"
+            value={formValues.password}
+            onChange={handleChange}
             type="password"
-            className="p-2 border border-black rounded-md input-bg w-full"
+            required
           />
-          <button className="mt-2 p-2 bg-color poppins rounded-xl font-bold w-full">
-            Register
+          <button
+            onClick={handleSubmit}
+            className="mt-2 p-2 bg-color poppins rounded-xl font-bold w-full"
+          >
+            Sign Up
           </button>
-        </div>
+          {error && "Something went wrong!"}
+        </form>
         <div
           className="absolute bottom-0 left-0 w-full h-full bg-black"
           style={{
